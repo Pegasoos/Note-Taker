3e2066fb51c52b70d054ecd445208b7d7c0a69e9
Pegasoos <circumambulation24@yahoo.com>
2021-10-10 20:59:35 -0400

    small edits for heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ router.get("/notes", (req, res) => {
 });
 
 // All other routes default to index.html
-router.get("*", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
